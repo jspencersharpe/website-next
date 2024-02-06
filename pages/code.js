4895705e5bdcbef08849972dd0581e8f16896de0
Spencer Sharpe <spencer.sharpe@hellotend.com>
2024-02-06 14:53:58 -0600

    Update code page

--- pages/code.js
+++ pages/code.js
@@ -1,18 +1,18 @@
-import Layout from '../components/Layout'
-import Technologies from '../components/Technologies'
-import Heading from '../components/Heading'
+import Layout from "../components/Layout";
+import Technologies from "../components/Technologies";
+import Heading from "../components/Heading";
 
 const Code = () => (
   <Layout>
     <div className="code">
       <Heading value="code" />
       <p>
-        I've been writing JavaScript since 2014. The majority of my experience
-        has been writing React, including{' '}
+        I've been writing software since 2014. The majority of my experience has
+        been writing JavaScript and React, including{" "}
         <a href="https://nextjs.org/" rel="noopener noreferrer" target="_blank">
           Next.js
-        </a>{' '}
-        and{' '}
+        </a>{" "}
+        and{" "}
         <a
           href="https://reactnative.dev/"
           rel="noopener noreferrer"
@@ -26,6 +26,6 @@ const Code = () => (
       <Technologies />
     </div>
   </Layout>
-)
+);
 
-export default Code
+export default Code;
--- utils.js
+++ utils.js
@@ -1,57 +1,20 @@
 export const technologies = [
   {
-    name: 'JavaScript',
+    name: "JavaScript",
     image_url:
-      'http://3.bp.blogspot.com/-PTty3CfTGnA/TpZOEjTQ_WI/AAAAAAAAAeo/KeKt_D5X2xo/s1600/js.jpg',
-    url: 'https://www.javascript.com/',
+      "http://3.bp.blogspot.com/-PTty3CfTGnA/TpZOEjTQ_WI/AAAAAAAAAeo/KeKt_D5X2xo/s1600/js.jpg",
+    url: "https://www.javascript.com/",
   },
   {
-    name: 'React',
+    name: "React",
     image_url:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png',
-    url: 'https://reactjs.org/',
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png",
+    url: "https://reactjs.org/",
   },
   {
-    name: 'Next.js',
+    name: "Next.js",
     image_url:
-      'https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2.0,f_auto,g_center,q_auto:good/v1/gcs/platform-data-dsc/events/nextjs-boilerplate-logo.png',
-    url: 'https://nextjs.org/',
+      "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2.0,f_auto,g_center,q_auto:good/v1/gcs/platform-data-dsc/events/nextjs-boilerplate-logo.png",
+    url: "https://nextjs.org/",
   },
-  {
-    name: 'Angular',
-    image_url: 'https://angular.io/assets/images/logos/angular/angular.png',
-    url: 'https://angular.io/',
-  },
-  {
-    name: 'HTML5',
-    image_url: 'https://www.w3.org/html/logo/downloads/HTML5_Logo_512.png',
-    url: 'https://en.wikipedia.org/wiki/HTML5',
-  },
-  {
-    name: 'Sass',
-    image_url: 'http://sass-lang.com/assets/img/logos/logo-b6e1ef6e.svg',
-    url: 'http://sass-lang.com/',
-  },
-  {
-    name: 'npm',
-    image_url:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Npm-logo.svg/2000px-Npm-logo.svg.png',
-    url: 'https://www.npmjs.com/',
-  },
-  {
-    name: 'git',
-    image_url: 'https://git-for-windows.github.io/img/git_logo.png',
-    url: 'https://git-scm.com/',
-  },
-  {
-    name: 'Webpack',
-    image_url: 'https://webpack.github.io/assets/logo.png',
-    url: 'https://webpack.github.io/',
-  },
-  {
-    name: 'Node',
-    image_url:
-      'https://raygun.com/blog/wp-content/uploads/2016/05/nodejs-logo.png',
-    url: 'https://nodejs.org/en/',
-  },
-]
+];
